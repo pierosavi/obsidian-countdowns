de6f27e1b2d1f2df342f9cb94f297893c2bdc587
Piero <15440183+pierosavi@users.noreply.github.com>
2026-03-09 21:51:18 +0100

    Update settings key case

--- src/settings.ts
+++ src/settings.ts
@@ -1,14 +1,14 @@
 import {App, PluginSettingTab, Setting} from "obsidian";
-import CountdownsPlugin from "./main";
+import type CountdownsPlugin from "./main";
 
 export interface CountdownsSettings {
-	countdowns_folder: string;
-	bases_folder: string;
+	countdownsFolder: string;
+	basesFolder: string;
 }
 
 export const DEFAULT_SETTINGS: CountdownsSettings = {
-	countdowns_folder: 'Countdowns',
-	bases_folder: 'Countdowns/Bases',
+	countdownsFolder: 'Countdowns',
+	basesFolder: 'Countdowns/Bases',
 }
 
 export class CountdownsSettingTab extends PluginSettingTab {
@@ -33,9 +33,9 @@ export class CountdownsSettingTab extends PluginSettingTab {
 			.setDesc('The folder where countdown notes will be created')
 			.addText(text => text
 				.setPlaceholder('Count down folder, e.g. "Countdowns/subfolder"')
-				.setValue(this.plugin.settings.countdowns_folder)
+				.setValue(this.plugin.settings.countdownsFolder)
 				.onChange(async (value) => {
-					this.plugin.settings.countdowns_folder = value;
+					this.plugin.settings.countdownsFolder = value;
 					await this.plugin.saveSettings();
 				}));
 
@@ -44,9 +44,9 @@ export class CountdownsSettingTab extends PluginSettingTab {
 			.setDesc('The folder where base notes will be created')
 			.addText(text => text
 				.setPlaceholder('Bases folder, e.g. "Countdowns/Bases"')
-				.setValue(this.plugin.settings.bases_folder)
+				.setValue(this.plugin.settings.basesFolder)
 				.onChange(async (value) => {
-					this.plugin.settings.bases_folder = value;
+					this.plugin.settings.basesFolder = value;
 					await this.plugin.saveSettings();
 				}));
 	}
